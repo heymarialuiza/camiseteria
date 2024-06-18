558d51dbbcf87ab8a62d850e6e8f43b3af1a3818
Maria Luiza B <heymarialuiza@gmail.com>
2024-06-18 10:06:59 -0400

    Configurações iniciais do projeto.

---
+++ src/test/java/com/demo/camiseteria/CamiseteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.camiseteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamiseteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
